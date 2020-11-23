4132ae40c5a4d9e8df7dfc36b8bf31910f4eb5b2
mingkhoi <mingkhoitran1234@gmail.com>
2020-11-24 03:56:03 +0700

    init chatbox

--- Frontend/bet-store-main/src/screen/home/home.tsx
+++ Frontend/bet-store-main/src/screen/home/home.tsx
@@ -1,13 +1,15 @@
+import classNames from "classnames";
 import { defaultCoreCipherList } from "constants";
 import React from "react";
 import { Link } from "react-router-dom";
+import style from "./home.module.scss";
 
 
 export default function Home() {
     return (
         <div className="hello">
             <h1 className="hello">Hello World</h1>
-            <Link className="login-cs" to="/login">Login</Link>
+            <Link className={style.loginCs} to="/login">Login</Link>
         </div>
     );
 } 
