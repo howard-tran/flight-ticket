d60106b1011f76bfdce0cfafb005ab1adf198a3c
mingkhoi <mingkhoitran1234@gmail.com>
2020-11-24 19:59:28 +0700

    silent task + stupid maven bug

--- Backend/JavaBackend/src/main/java/com/service/MessageService.java
+++ Backend/JavaBackend/src/main/java/com/service/MessageService.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService implements LogService {
+
   private IMessageDao messageDao;
   private IConversationDao conversationDao;
 
