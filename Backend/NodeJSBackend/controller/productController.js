4e65737a14c731784115849dd5c42c9354773026
minhtien2152 <18521481@gm.uit.edu.vn>
2020-11-26 23:42:30 +0700

    GET product according to request body

--- Backend/NodeJSBackend/controller/productController.js
+++ Backend/NodeJSBackend/controller/productController.js
@@ -18,3 +18,23 @@ export const createProduct = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
+
+export const getProducts = asyncHandler(async (req, res) => {
+  let products;
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    products = await Product.find({});
+  } else {
+    products = await Product.find(req.body);
+  }
+  res.json(products);
+});
+
+export const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+  }
+});
--- Backend/NodeJSBackend/routes/productRoute.js
+++ Backend/NodeJSBackend/routes/productRoute.js
@@ -1,37 +1,24 @@
 import express from "express";
-import Product from "../models/productModel.js";
-import asyncHandler from "express-async-handler";
-import { createProduct } from "../controller/productController.js";
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+} from "../controller/productController.js";
 const router = express.Router();
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.get("/", getProducts);
 
-// @desc Fetch single products
+// @desc Fetch single product
 // @route GET /api/products/:id
 // @access Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+router.get("/:id", getProductById);
 
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
-
+// @desc Create single product
+// @route POST /api/products/
+// @access User only
 router.post("/", createProduct);
 
 export default router;
