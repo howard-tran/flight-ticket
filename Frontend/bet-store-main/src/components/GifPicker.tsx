03246e18046f8136f4f5dd638b4aeef0d126456c
mingkhoi <mingkhoitran1234@gmail.com>
2020-12-18 08:47:08 +0700

    grid giphy

--- Frontend/bet-store-main/src/components/GifPicker.tsx
+++ Frontend/bet-store-main/src/components/GifPicker.tsx
@@ -1,4 +1,4 @@
-// @ts-nocheck
+
 import React, { useState } from "react";
 import {GiphyFetch} from "@giphy/js-fetch-api";
 import { IGif } from "@giphy/js-types";
@@ -50,12 +50,13 @@ const _GifPicker: React.FC<{onGifChoosen: (gif: any, e : any) => void}> = (param
 
   return ( 
     <div style={{backgroundColor:"whitesmoke", height:"450px", maxHeight:"450px", padding:"10px"}}>
-      <div style={{textAlign:"center", height:"10%", display:"flex"}}>
-        <textarea style={{resize:"none"}} rows={1} onInput={
-          (e : React.FormEvent<HTMLTextAreaElement>) => {
-            setSearchString((e.target as HTMLTextAreaElement).value);
+      <div style={{height:"10%", display:"flex", flexFlow:"row", alignItems:"center"}}>
+        <i className="fa fa-search fa-1x" style={{paddingRight:"5px"}}></i>
+        <input style={{resize:"none"}} onChange={
+          (e : React.ChangeEvent<HTMLInputElement>) => {
+            setSearchString((e.target as HTMLInputElement).value);
           }
-        }></textarea>
+        }></input>
       </div>
       {renderHandle()}
     </div>
